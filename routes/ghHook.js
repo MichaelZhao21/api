8f29eb8ba2dc969df5af0ee1afa6f59679283323
MichaelZhao21 <michaelzhao314@gmail.com>
2020-07-13 18:02:58 -0500

    ugh ill do my own post req

--- routes/ghHook.js
+++ routes/ghHook.js
@@ -1,24 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var createHandler = require('github-webhook-handler');
-var handler = createHandler({ path: '/pull' });
 
-router.get('/', function (req, res, next) {
+router.get('/', function(req, res, next) {
     res.send('github webhooks here');
-    handler(req, res, function (err) {
-        res.statusCode = 404
-        res.end('no such location')
-    })
 });
 
-handler.on('error', function (err) {
-    console.error("Error: ", err.message);
-});
-
-handler.on('push', function (event) {
-    console.log('Received a push event for %s to %s',
-        event.payload.repository.name,
-        event.payload.ref)
+router.post('/pull', function (req, res, next) {
+    console.log(req.body);
+    res.statusCode(200);
 });
 
 module.exports = router;
