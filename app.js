67a1faea20d2382a6c82e6975dff41d44bfff7a2
MichaelZhao21 <michaelzhao314@gmail.com>
2020-07-13 17:49:08 -0500

    WHO ADDED ANOTHER APP.JS TUWFHWRGBIUOEWF

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ var app = express();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testAPIRouter = require('./routes/testAPI');
-var postUpRouter = require('./routes/postUp');
+var postUpRouter = require('./routes/postUp'); // TODO: doesn't work bc google project is gone rip
+var ghHookRouter = require('./routes/ghHook');
 
-app.use(express.json());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api/testAPI', testAPIRouter);
+app.use('/testAPI', testAPIRouter);
 app.use('/postup', postUpRouter);
+app.use('/gh-hook', ghHookRouter);
 
-// app.listen(8080, '172.31.62.232');
 app.listen(8080);
