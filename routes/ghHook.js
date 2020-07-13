bffe40e518e9cd2cb951ee40e25a1570f745d497
MichaelZhao21 <michaelzhao314@gmail.com>
2020-07-13 18:01:32 -0500

    yeah that package was bad

--- routes/ghHook.js
+++ routes/ghHook.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var createHandler = require('github-webhook-handler');
+var handler = createHandler({ path: '/pull' });
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     res.send('github webhooks here');
+    handler(req, res, function (err) {
+        res.statusCode = 404
+        res.end('no such location')
+    })
+});
+
+handler.on('error', function (err) {
+    console.error("Error: ", err.message);
+});
+
+handler.on('push', function (event) {
+    console.log('Received a push event for %s to %s',
+        event.payload.repository.name,
+        event.payload.ref)
 });
 
 module.exports = router;
