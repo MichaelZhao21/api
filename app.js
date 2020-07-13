ad9fa266076d1cb8933ab06c2297bda979194212
MichaelZhao21 <michaelzhao314@gmail.com>
2020-07-13 18:41:03 -0500

    not using gh hooks anymore

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testAPIRouter = require('./routes/testAPI');
 var postUpRouter = require('./routes/postUp'); // TODO: doesn't work bc google project is gone rip
-var ghHookRouter = require('./routes/ghHook');
 
 app.use(bodyParser.json());
 
@@ -14,6 +13,5 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/testAPI', testAPIRouter);
 app.use('/postup', postUpRouter);
-app.use('/gh-hook', ghHookRouter);
 
 app.listen(8080);
