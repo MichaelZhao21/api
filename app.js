1f12b182777a7f26fde4ed3ae1257373809883ab
Michael Zhao <michaelzhao314@gmail.com>
2020-11-01 01:50:01 -0500

    Removed postup route

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var app = express();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testAPIRouter = require('./routes/testAPI');
-var postUpRouter = require('./routes/postUp'); // TODO: doesn't work bc google project is gone rip
 var todoRouter = require('./routes/todo');
 var photoRouter = require('./routes/photo');
 
