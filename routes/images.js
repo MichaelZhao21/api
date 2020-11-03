1b2cf2e387e205efd8f235c93e7afc4099154bac
Michael Zhao <michaelzhao314@gmail.com>
2020-11-03 01:55:06 -0600

    Added upload image endpoint

--- routes/images.js
+++ routes/images.js
@@ -4,8 +4,7 @@ const Dropbox = require('dropbox');
 const creds = require('./files/creds.json');
 const dbx = new Dropbox.Dropbox({ accessToken: creds.dropbox.accessToken });
 
-// TODO: Add upload path for only me
-router.get(/\/.*/, function (req, res, next) {
+router.get(/\/.*[^upload-callback]/, function (req, res, next) {
     console.log(`GET /images${req.path}`);
     dbx.filesDownload({ path: req.path })
         .then((data) => {
@@ -17,6 +16,32 @@ router.get(/\/.*/, function (req, res, next) {
                 request: req.path,
                 error: '400 Bad Request',
                 description: `File not found at ${req.path}`,
+            });
+        });
+});
+
+router.post('/upload', function (req, res, next) {
+    res.setHeader('Content-Type', 'application/json');
+
+    if (req.body.image === undefined || req.body.path === undefined) {
+        res.status(400);
+        res.send({
+            request: req.body,
+            error: '400 Bad Request',
+            description: `Request did not include 'image' and 'path' fields`,
+        });
+    }
+
+    dbx.filesUpload({ path: req.body.path, contents: req.body.image })
+        .then(() => {
+            res.sendStatus(200);
+        })
+        .catch((err) => {
+            res.status(400);
+            res.send({
+                request: req.body,
+                error: err,
+                description: `Failed Dropbox API Call: Check your image blob format or path`,
             });
         });
 });
