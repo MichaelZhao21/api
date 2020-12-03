9319101d6795ac8dcd0bd4785465742e61138ccd
MichaelZhao21 <michaelzhao314@gmail.com>
2020-12-02 20:29:52 -0600

    Fixed ip get on server

--- routes/logging.js
+++ routes/logging.js
@@ -5,8 +5,10 @@ var router = express.Router();
 var now = require('./now');
 
 router.get('/', function (req, res, next) {
-    var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
-    fs.appendFile(path.join(__dirname, 'files', 'logging', 'test'), `[${now()}] ${ip}`, () => {});
+    // Need to add to nginx config:
+    // proxy_set_header X-Real-IP $remote_addr;
+    var ip = req.header('X-Real-IP') || req.connection.remoteAddress;
+    fs.appendFile(path.join(__dirname, 'files', 'logging', 'test'), `[${now()}] ${ip}\n`, () => {});
     res.send("hi");
 });
 
