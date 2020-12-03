faf2ed1edc9db7ffd433f548b31ca38a746d3e24
Michael Zhao <michaelzhao314@gmail.com>
2020-12-02 21:02:19 -0600

    Changed logging to POST request

--- routes/logging.js
+++ routes/logging.js
@@ -4,12 +4,19 @@ var fs = require('fs');
 var router = express.Router();
 var now = require('./now');
 
-router.get('/', function (req, res, next) {
+router.post('/', function (req, res, next) {
+    if (!req.body.dir) {
+        res.status(400);
+        res.send({
+            status: 400,
+            error: "Missing 'dir' in request body",
+        });
+    }
     // Need to add to nginx config:
     // proxy_set_header X-Real-IP $remote_addr;
     var ip = req.header('X-Real-IP') || req.connection.remoteAddress;
-    fs.appendFile(path.join(__dirname, 'files', 'logging', 'test'), `[${now()}] ${ip}\n`, () => {});
-    res.send("hi");
+    fs.appendFile(path.join(__dirname, 'files', 'logging', req.body.dir), `[${now()}] ${ip}\n`, () => {});
+    res.sendStatus(200);
 });
 
 module.exports = router;
